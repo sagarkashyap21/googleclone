f4099dda6910e0f5803bfa35dffbbaf3da8475c6
sagar kashyap <kashyapsagarks21@gmail.com>
2023-01-22 13:50:38 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Menu from './Component/Menu'
 import Navbar from "./Component/Navbar"
 import Section from './Component/Section'
+import "./App.css"
 
 export default function App() {
   return (
