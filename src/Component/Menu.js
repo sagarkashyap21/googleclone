9d85d212068a670a4eb882023fb59210bed5ac03
sagar kashyap <kashyapsagarks21@gmail.com>
2023-01-24 01:43:09 +0530

    responive1 commit

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -3,31 +3,31 @@ import "../App.css"
 
 
 export default function Menu() {
-    const removeIcons=()=>{
-        let id=document.getElementById("menu");
-        id.className="menu";
+    const removeIcons = () => {
+        let id = document.getElementById("menu");
+        id.className = "menu";
         console.log(id.className);
-        }
+    }
     return (
-        <div id="menu" className="menu" style={{ border: "",width:"370px",position:"absolute",top:"70px",right:"-40px" }}>
-            <div id="Wrong" onClick={removeIcons} style={{border:"",width:"100px",marginLeft:"127px",display:"flex",justifyContent:"center"}}><i  style={{fontSize:"30px",color:"gray"}} class="fa fa-regular fa-circle-xmark"></i></div>
+        <div id="menu" className="menu" style={{ border: "",backgroundColor:"white", width: "370px", position: "absolute", top: "70px", right: "-40px" }}>
+            <div id="Wrong" onClick={removeIcons} style={{ border: "", width: "100px", marginLeft: "127px", display: "flex", justifyContent: "center" }}><i style={{ fontSize: "30px", color: "gray" }} class="fa fa-regular fa-circle-xmark"></i></div>
             <ul className="menuUl">
-     <a  style={{textDecoration:"none",color:"black"}}>  <li><img src="https://cdn-icons-png.flaticon.com/512/61/61205.png"  alt="" />Account</li></a>         
-               <a href="https://mail.google.com/" style={{textDecoration:"none",color:"black"}}> <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png" alt="" />Gmail</li></a>
-               <a href="https://www.youtube.com/" style={{textDecoration:"none",color:"black"}}><li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/800px-YouTube_full-color_icon_%282017%29.svg.png" alt="" />Youtube</li></a>
-                
+                <a style={{ textDecoration: "none", color: "black" }}>  <li><img src="https://cdn-icons-png.flaticon.com/512/61/61205.png" alt="" />Account</li></a>
+                <a href="https://mail.google.com/" style={{ textDecoration: "none", color: "black" }}> <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png" alt="" />Gmail</li></a>
+                <a href="https://www.youtube.com/" style={{ textDecoration: "none", color: "black" }}><li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/800px-YouTube_full-color_icon_%282017%29.svg.png" alt="" />Youtube</li></a>
+
                 <a> <li><img src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Google_Contacts_logo.png" alt="" />Contacts</li></a>
-               
-               <a style={{textDecoration:"none",color:"black"}} href="https://meet.google.com/?pli=1"><li><img src="https://1000logos.net/wp-content/uploads/2021/12/Google-Meet-Logo-500x281.png" alt="" />Meet</li></a>
+
+                <a style={{ textDecoration: "none", color: "black" }} href="https://meet.google.com/?pli=1"><li><img src="https://1000logos.net/wp-content/uploads/2021/12/Google-Meet-Logo-500x281.png" alt="" />Meet</li></a>
                 <a >  <li><img src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg" alt="" />Search</li></a>
 
-              <a style={{textDecoration:"none",color:"black"}} href="https://www.google.com/maps"> <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Google_Maps_icon_%282015-2020%29.svg/512px-Google_Maps_icon_%282015-2020%29.svg.png?20200220195824" alt="" />Maps</li></a>
-               
-               <a style={{textDecoration:"none",color:"black"}} href="https://play.google.com/store/games?pli=1">  <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Play_Arrow_logo.svg/512px-Google_Play_Arrow_logo.svg.png?20160711072735" alt="" />Play</li></a>
-              
-              <a style={{textDecoration:"none",color:"black"}} href="https://www.google.com/intl/en_in/drive/">  <li><img src="https://zeevector.com/wp-content/uploads/Google-Drive-Logo-Transparent.png" alt="" />Drive</li></a>
-              
-                
+                <a style={{ textDecoration: "none", color: "black" }} href="https://www.google.com/maps"> <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Google_Maps_icon_%282015-2020%29.svg/512px-Google_Maps_icon_%282015-2020%29.svg.png?20200220195824" alt="" />Maps</li></a>
+
+                <a style={{ textDecoration: "none", color: "black" }} href="https://play.google.com/store/games?pli=1">  <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Play_Arrow_logo.svg/512px-Google_Play_Arrow_logo.svg.png?20160711072735" alt="" />Play</li></a>
+
+                <a style={{ textDecoration: "none", color: "black" }} href="https://www.google.com/intl/en_in/drive/">  <li><img src="https://zeevector.com/wp-content/uploads/Google-Drive-Logo-Transparent.png" alt="" />Drive</li></a>
+
+
 
             </ul>
 
--- src/Component/Section.js
+++ src/Component/Section.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import "../App.css"
 export default function Section() {
   const [data, setdata] = useState(null)
 
@@ -26,18 +27,18 @@ export default function Section() {
 
   }
   return (
-    <div style={{}}>
-      <img id="img" style={{ height: "200px", width: "400px", marginTop: "20px", display: "block", margin: "auto" }} src="http://www.golegal.co.za/wp-content/uploads/2015/09/google-logo.jpg" alt="" />
+    <div style={{border:"",width:""}}>
+      <img className="img" style={{border:"", marginTop: "20px",}} src="https://cdn.pixabay.com/photo/2015/10/31/12/54/google-1015751__340.png" alt="" />
 
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <ul style={{ border: "1px solid lightgray", boxShadow: "2px 2px lightgray", height: "40px", borderRadius: "20px", listStyle: "none", display: "flex", alignItems: "center" }}>
+      <div style={{border:"" ,display: "flex", justifyContent: "center" }}>
+        <ul className="sectionUl" style={{ border: "1px solid lightgray", boxShadow: "2px 2px lightgray", height: "40px", borderRadius: "20px", listStyle: "none", display: "flex", alignItems: "center" }}>
           <li><i onClick={google} style={{ color: "gray", marginRight: "10px" }} class="fa fa-solid fa-magnifying-glass"></i></li>
-          <li><input id="input" style={{ width: "500px", border: "0 solid", outline: "none" }} type="text" placeholder="Search Google or type a URL" onChange={Input} onKeyDown={Search} /></li>
+          <li id="input"><input  style={{ width: "500px", border: "0 solid", outline: "none" }} type="text" placeholder="Search Google" onChange={Input} onKeyDown={Search} /></li>
           <li><i style={{ marginRight: "30px", marginLeft: "10px" }} class="fa fa-solid fa-microphone"></i></li>
         </ul>
       </div>
-      <ul style={{display:"flex",listStyle:"none",justifyContent:"center"}}>
-        <li><button style={{border:"0",height:"30px",borderRadius:"20px"}}>Google Search</button></li>
+      <ul className="sectionButton" style={{border:"",display:"flex",listStyle:"none",justifyContent:"center"}}>
+        <li className="sb1"><button style={{border:"0",height:"30px",borderRadius:"20px"}}>Google Search</button></li>
         <li><button style={{border:"0",marginLeft:"8px",height:"30px",borderRadius:"20px"}}>I'm Feeling Lucky</button></li>
       </ul>
 
